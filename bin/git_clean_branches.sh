0c786515fe5ca58e43d1e7e8bc52f9aaf4c73a54
Michael Hale <mike@hales.ws>
2013-02-12 13:53:23 -0500

    Revert "Revert "incorporate local stashed changes""
    This reverts commit 3365c0045280be9cd43bb009489ca8ac3a4117a7.

--- bin/git_clean_branches.sh
+++ bin/git_clean_branches.sh
@@ -6,5 +6,5 @@ git checkout master
 git pull origin master
 for branch in `git branch --merged | egrep -v '\*|v[0-9]+$' | tr -d ' '`; do
   git branch -d $branch
-  git push origin :$branch
+  git push origin :$branch || true
 done
