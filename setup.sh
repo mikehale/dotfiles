9544bdf654b2676ed9674fec7644e5fd043a6a68
Michael Hale <mike@hales.ws>
2014-03-25 16:10:14 -0400

    fonts

--- setup.sh
+++ setup.sh
@@ -5,3 +5,6 @@ brew install tmux
 # fix pbcopy
 brew install reattach-to-user-namespace --wrap-launchctl --wrap-pbcopy-and-pbpaste
 
+# fonts
+
+cp fonts/AnonymousPro-1.002.001/*.ttf ~/Library/Fonts
