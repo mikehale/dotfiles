fa974c7b8b3f82c08c28097c398a8d4006e4f4eb
Michael Hale <mike@hales.ws>
2014-03-31 23:46:30 -0400

    x

--- setup.sh
+++ setup.sh
@@ -5,6 +5,13 @@ brew install tmux
 # fix pbcopy
 brew install reattach-to-user-namespace --wrap-launchctl --wrap-pbcopy-and-pbpaste
 
+# support dired
+brew install coreutils
+
 # fonts
 
 cp fonts/AnonymousPro-1.002.001/*.ttf ~/Library/Fonts
+
+# terminal info
+
+tic -o ~/.terminfo /usr/local/Cellar/emacs/24.3/share/emacs/24.3/etc/e/eterm-color.ti
