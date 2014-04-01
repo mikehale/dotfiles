cc00c395301264eed7105f6d65679f3b8ccf95ca
Michael Hale <mike@hales.ws>
2014-04-01 14:06:10 -0400

    document emacs daemon setup

--- setup.sh
+++ setup.sh
@@ -15,3 +15,8 @@ cp fonts/AnonymousPro-1.002.001/*.ttf ~/Library/Fonts
 # terminal info
 
 tic -o ~/.terminfo /usr/local/Cellar/emacs/24.3/share/emacs/24.3/etc/e/eterm-color.ti
+
+# daemon
+cp setup/emacs-server-wrapper /usr/local/bin
+sudo cp setup/gnu.emacs.daemon.plist /Library/LaunchAgents/
+sudo launchctl load -w /Library/LaunchAgents/gnu.emacs.daemon.plist
