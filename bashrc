667cb0c761ee77a3ff8276933625951964effecb
Michael Hale <mhale@Michaels-Air.lan>
2016-06-24 11:08:45 -0400

    fresh start

--- bashrc
+++
@@ -1,2 +0,0 @@
-# Use ~/.bashrc for things that you want around in subshells that wouldn't
-# otherwise be inherited. So, aliases, completions, and functions.
--- bin/ccc_post_flight.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-(sleep 60; diskutil unmount "$2" >> /Library/Logs/CCC.log 2>&1) &
-(/usr/libexec/PlistBuddy -c "Print" /Library/Logs/CCC.stats | tail -n 9 | mail -s "CCC Scheduled Task Report" mike@hales.ws) &
